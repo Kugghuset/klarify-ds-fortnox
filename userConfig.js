9fb962301817304d8bbb1658b628aa4f6a1d7ce6
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-10-14 17:43:16 +0200

    Remove userConfig.js

--- userConfig.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = {
-  dbUser: 'sa', // database user
-  dbPass: 'pass', // database password
-  dbServer: 'localhost', // database server
-  dbName: 'dbName' // name of database
-};
