0e6738e0d72d58d8944caa9250cb9ff638f520dc
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-10-14 17:55:01 +0200

    Temporary upate gitignore and re add userConfig

---
+++ userConfig.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+  dbUser: 'sa', // database user
+  dbPass: 'pass', // database password
+  dbServer: 'EASTGROVESOFTWA\\LOCALSQL', // database server
+  dbName: 'dbName' // name of database
+};
