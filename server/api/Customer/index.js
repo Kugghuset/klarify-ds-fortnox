ff87d5e0d15e93462bad1f41801e5f0f1a4a7817
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-10-29 17:22:49 +0100

    Fix capitalization error in index file of Customer

--- server/api/Customer/index.js
+++ server/api/Customer/index.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var router = express.Router();
 
-var customer = require('./Customer');
+var customer = require('./customer');
 
 /**
  * Triggers the cleanAndFetch flow method.
